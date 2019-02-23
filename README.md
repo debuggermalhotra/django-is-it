05cd4c789f1994e430a01cabf556984d669c82c6
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2019-02-23 23:00:26 +0530

    commit

---
+++ README.md
@@ -0,0 +1,8 @@
+1. django-admin startproject my-project -- for creating a project
+2. django-admin startapp my-app -- for creating an app
+3. python manage.py runserver -- for running the app
+4. python manage.py makeigrations -- to initate migrations
+5. python manage.py sqlmigrate boards 0001 -- view sql instructions of migrations
+6. python manage.py migrate -- apply migration to database
+7. python manage.py shell -- use app in shell
+8. python manage.py test -- execute the Django’s test suite:
---
+++ myproject/boards/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse,Http404
+from django.shortcuts import get_object_or_404,render,redirect
+from .models import Board,Topic,Post
+from django.contrib.auth.models import User
+# Create your views here.
+
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404
+    return render(request, 'topics.html', {'board': board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    if request.method=='POST':
+        subject=request.POST['subject']
+        message=request.POST['message']
+        user=User.objects.first()
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user
+        )
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+        return redirect('board_topics', pk=board.pk)
+    return render(request, 'new_topic.html', {'board': board})
+
