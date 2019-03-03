7e30b09fb76d1a8971ee730bd71aa0487e5c1159
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2019-03-03 22:54:40 +0530

    commit

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'boards',
-    'widget_tweaks'
+    'widget_tweaks',
+     'accounts'
 ]
 
 MIDDLEWARE = [
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,re_path
 from boards import views
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    re_path(r'^signup/$', accounts_views.signup, name='signup'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 ]
