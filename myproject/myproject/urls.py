13a9528ef42af69b8f97a7921479930e6f6927b9
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2019-03-08 23:00:19 +0530

    commit

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -34,4 +34,13 @@ urlpatterns = [
             subject_template_name='password_reset_subject.txt'
         ),
         name='password_reset'),
+    re_path(r'^reset/done/$',
+    auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+    name='password_reset_done'),
+    re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+    name='password_reset_confirm'),
+    re_pathurl(r'^reset/complete/$',
+    auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+    name='password_reset_complete')
 ]
